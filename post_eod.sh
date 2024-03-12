d1b3d793de7fdad8d86a0082223409208c4b0e06
QuocThinhPham <thinhphamk8@gmail.com>
2024-03-12 12:00:36 +0700

    updated_20240312

--- post_eod.sh
+++ post_eod.sh
@@ -8,7 +8,7 @@
 # * -
 # * - create_restore_point.sh $user $pass $db $rp_name
 # * - return "Created" | "Failed"
-#
+# *
 # 2. check R2 is created?
 ### 2.1. created: continue
 ### 2.2. not created: failed
@@ -18,20 +18,20 @@
 # 3. activate stby to primary (check database_role, open_mode)
 # * - activate_stby.sh $user $pass $db
 # * - return "Activate $db: Successfully" | "Activate $db: Successfully"
-#
+# *
 # 4. run eod
 # 5. check R2 is existed, then flashback to R2
 ### 5.1. Existed: continue
 ### 5.2. Not: failed
 # * - get_restore_point $user $pass $db $rp_name
 # * - return "OK" | "Failed"
-#
+# *
 # 6. check current scn = flashback scn
 ### 6.1. current scn == flashback scn: continue
 ### 6.2. current scn != flashback scn: failed
 # * - compare_scn $user $pass $db $rp_name
 # * - return "OK" | "Failed"
-#
+# *
 # 7. start transport and apply redo and check lag
 # * - start_apply.sh $user $pass $db
 # *** - check_lag.sh
