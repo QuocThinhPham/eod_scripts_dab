2a3c1fb005a8cf27c894099098bcb51b4ca0e1f3
QuocThinhPham <thinhphamk8@gmail.com>
2024-03-18 15:43:26 +0700

    [update_2024Mar18] run main scripts: post-eod

---
+++ dab_scripts/post_eod_revert_stby.sh
@@ -0,0 +1,18 @@
+# sh post_eod_revert_stby.sh
+source ./constants.sh
+source ./utils.sh
+source ./dataguard.sh
+source ./restore_point.sh
+source ./database.sh
+
+. /home/oracle/db10g_env fcclive
+
+now=`date`
+echo "~~~~~~~~~ $now ~~~~~~~~~"
+
+#
+echo "View log file: tail -500f $LOG_PATH"
+echo "" > "$LOG_PATH"
+
+# f_db_activate_stby_to_primary "sys" "oracle" "fccreport"
+f_db_revert_to_stby "sys" $pass "fccreport"
